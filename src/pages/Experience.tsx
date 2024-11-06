e24c75bd78d0597016a1cb45df927133331bf7ab
Luka Talakvadze <lukatalakvadze@gmail.com>
2024-11-06 14:31:56 +0400

    WIP: add functionality that adds new module of experience

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -15,27 +15,58 @@ import GobackButton from "../components/GobackButton";
 import CvComponent from "../components/CvComponent";
 import { Link } from "react-router-dom";
 import { useFormData } from "../Context/FormDataContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Experience() {
-  const {
-    position,
-    employer,
-    startDate,
-    finishDate,
-    jobDescription,
-    setFormData,
-    uploadedImgUrl,
-  } = useFormData();
+  const { uploadedImgUrl } = useFormData();
 
+  // const handleChange = (
+  //   e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  // ) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [name]: value,
+  //   }));
+  // };
+
+  const [experiences, setExperiences] = useState([
+    {
+      id: Date.now(),
+      position: "",
+      employer: "",
+      startDate: "",
+      finishDate: "",
+      jobDescription: "",
+    },
+  ]);
+
+  // Function to add a new experience module
+  const handleAddExperience = () => {
+    setExperiences([
+      ...experiences,
+      {
+        id: Date.now(),
+        position: "",
+        employer: "",
+        startDate: "",
+        finishDate: "",
+        jobDescription: "",
+      },
+    ]);
+  };
+
+  // Function to handle input changes for each experience
   const handleChange = (
+    id: number,
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+    setExperiences((prevExperiences) =>
+      prevExperiences.map((exp) =>
+        exp.id === id ? { ...exp, [name]: value } : exp
+      )
+    );
   };
 
   useEffect(() => {
@@ -48,65 +79,73 @@ function Experience() {
         <GobackButton previousLocation={"/personal-info"} />
         <Content>
           <Header pageTitle={"გამოცდილება"} PageNumber={"2"} />
-          <Form>
-            <InputDiv>
-              <StyledLabel>თანამდებობა</StyledLabel>
-              <StyledInput
-                placeholder="დეველოპერი, დიზაინერი და ა.შ"
-                type="text"
-                name="position"
-                value={position}
-                onChange={handleChange}
-              />
-              <StyledHint>მინიმუმ 2 სიმბოლო</StyledHint>
-            </InputDiv>
-          </Form>
-          <EmployerDiv>
-            <InputDiv>
-              <StyledLabel>დამსაქმებელი</StyledLabel>
-              <StyledInput
-                placeholder="მიუთითეთ დამსაქმებელი"
-                type="text"
-                name="employer"
-                value={employer}
-                onChange={handleChange}
-              />
-              <StyledHint>მინიმუმ 2 სიმბოლო</StyledHint>
-            </InputDiv>
-          </EmployerDiv>
-          <DatesForm>
-            <InputDiv>
-              <StyledLabel>დაწყების თარიღი</StyledLabel>
-              <StyledInput
-                type="date"
-                name="startDate"
-                value={startDate}
-                onChange={handleChange}
-              />
-            </InputDiv>
-            <InputDiv>
-              <StyledLabel>დამთავრების თარიღი</StyledLabel>
-              <StyledInput
-                type="date"
-                name="finishDate"
-                value={finishDate}
-                onChange={handleChange}
-              />
-            </InputDiv>
-          </DatesForm>
-          <div className="flex-col mt-30 w-full">
-            <StyledLabel>თანამდებობის აღწერა</StyledLabel>
-            <TextArea
-              name="jobDescription"
-              value={jobDescription}
-              onChange={handleChange}
-            />
-            <StyledHint style={{ marginTop: "8px" }}>
-              მაქსიმუმ 300 სიტყვა
-            </StyledHint>
-          </div>
-          <SectionLine style={{ marginTop: "50px" }} />
-          <GeneralButton style={{ marginTop: "45px" }} buttonBgColor="#62a1eb">
+          {experiences.map((experience, index) => (
+            <div className="w-full" key={experience.id}>
+              <Form>
+                <InputDiv>
+                  <StyledLabel>თანამდებობა</StyledLabel>
+                  <StyledInput
+                    placeholder="დეველოპერი, დიზაინერი და ა.შ"
+                    type="text"
+                    name="position"
+                    value={experience.position}
+                    onChange={(e) => handleChange(experience.id, e)}
+                  />
+                  <StyledHint>მინიმუმ 2 სიმბოლო</StyledHint>
+                </InputDiv>
+              </Form>
+              <EmployerDiv>
+                <InputDiv>
+                  <StyledLabel>დამსაქმებელი</StyledLabel>
+                  <StyledInput
+                    placeholder="მიუთითეთ დამსაქმებელი"
+                    type="text"
+                    name="employer"
+                    value={experience.employer}
+                    onChange={(e) => handleChange(experience.id, e)}
+                  />
+                  <StyledHint>მინიმუმ 2 სიმბოლო</StyledHint>
+                </InputDiv>
+              </EmployerDiv>
+              <DatesForm>
+                <InputDiv>
+                  <StyledLabel>დაწყების თარიღი</StyledLabel>
+                  <StyledInput
+                    type="date"
+                    name="startDate"
+                    value={experience.startDate}
+                    onChange={(e) => handleChange(experience.id, e)}
+                  />
+                </InputDiv>
+                <InputDiv>
+                  <StyledLabel>დამთავრების თარიღი</StyledLabel>
+                  <StyledInput
+                    type="date"
+                    name="finishDate"
+                    value={experience.finishDate}
+                    onChange={(e) => handleChange(experience.id, e)}
+                  />
+                </InputDiv>
+              </DatesForm>
+              <div className="flex-col mt-30 w-full">
+                <StyledLabel>თანამდებობის აღწერა</StyledLabel>
+                <TextArea
+                  name="jobDescription"
+                  value={experience.jobDescription}
+                  onChange={(e) => handleChange(experience.id, e)}
+                />
+                <StyledHint style={{ marginTop: "8px" }}>
+                  მაქსიმუმ 300 სიტყვა
+                </StyledHint>
+              </div>
+              <SectionLine style={{ marginTop: "50px" }} />
+            </div>
+          ))}
+          <GeneralButton
+            onClick={handleAddExperience}
+            style={{ marginTop: "45px" }}
+            buttonBgColor="#62a1eb"
+          >
             მეტი გამოცდილების დამატება
           </GeneralButton>
           <NavigationBtnsDiv>
